bc8cfeb5c994af0c4e1e5766abd13a872c5d628c
Nick Lombardo <lombardon@uindy.edu>
2016-03-11 20:17:22 -0500

    payroll upgrade

--- payroll.cpp
+++ payroll.cpp
@@ -1,9 +1,13 @@
+// This progam calculates the user's pay. 
 #include <iostream>
+#include <fstream>
 using namespace std;
+
  
 int main() { 
 //setting up variables
-double hours, rate, pay; 
+double hours, rate, grosspay, netpay, ftax, stax; 
+ofstream outputFile;
  
 // Get the number of hours worked. 
 cout << "How many hours did you work? "; 
@@ -12,11 +16,32 @@ cin >> hours;
 // Get the hourly pay rate. 
 cout << "How much do you get paid per hour? "; 
 cin >> rate; 
- 
-// Calculate the pay. 
-pay = hours * rate; 
+  
+// Get the percent paid to federal tax.
+cout << "What perecnt do you pay to federal tax? ";
+cin >> ftax;
+  
+// Get the percent paid to state tax.
+cout << "What percent to you pay to state tax? ";
+cin >> stax;
+
+// Open a file
+outputFile.open("Payroll.txt");
+
+// Calculate the gross pay. 
+grosspay = hours * rate; 
+
+// Calculate the net pay.
+netpay = (hours * rate) * ((100-ftax)/100) * ((100-stax)/100);
+  
+// Write to the file
+outputFile << grosspay << endl;
  
 // Display the pay. 
-cout << "You have earned $" << pay << endl; 
+cout << "Your gross pay is $" << grosspay << endl;
+cout << "Your net pay is $" << netpay << endl;
+
+// close the file
+outputFile.close();
 return 0; 
 }
