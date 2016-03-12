d2b2b83e60d2ab155240bd27a9a3abf050cf5971
Nick Lombardo <lombardon@uindy.edu>
2016-03-11 20:23:34 -0500

    Racecar upgrade

--- Racecar.cpp
+++ Racecar.cpp
@@ -1,11 +1,13 @@
 // RACECARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 int main() {
 // Setting up variables
 double distance, time, speed, laps;
+ofstream outputFile;
   
 // Get the time
 cout << "For how much time, in minutes, was the car moving? ";
@@ -15,14 +17,23 @@ cin >> time;
 cout << "What was the average speed of the car? ";
 cin >> speed;
 
+// open a file
+outputFile.open("Miles.txt");
+  
 // Calculate the distance traveled
 distance = speed * (time / 60);
 
 // Calculate the number of laps traveled
 laps = distance / 2.5;
 
+// write to the file 
+outputFile << distance << endl;
+
 // Display the distance
 cout << "The number of miles traveled is " << distance << endl;
 cout << "The number of laps traveled is " << laps << endl;
+
+// close the file
+outputFile.close();
 return 0;
 }
